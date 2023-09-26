46e319a925d4843f968b26fc4af58d2d3d8d2bb7
Guilherme Lamim <guilherme.lamim96@gmail.com>
2023-09-26 15:55:25 -0300

    UserDetailsService
    configuração usada para recuperar usuario por e-mail ou id para autenticação do próprio atributo configurado

---
+++ src/main/java/com/novidades/gestaodeprojetos/security/CustomUserDetailService.java
@@ -0,0 +1,35 @@
+package com.novidades.gestaodeprojetos.security;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.novidades.gestaodeprojetos.model.Usuario;
+import com.novidades.gestaodeprojetos.service.UsuarioService;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        Usuario usuario = getUser(() -> usuarioService.obterPorEmail(email));
+        return usuario;
+    }
+
+    public UserDetails obterUsuarioPorId(Long id) {
+        return usuarioService.obterPorId(id).get();
+    }
+
+    private Usuario getUser(Supplier<Optional<Usuario>> supplier) {
+        return supplier.get().orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado!"));
+    }
+
+}
