6cdd668934141f42117481d6c6e57ae1f90e60da
Guilherme Lamim <guilherme.lamim96@gmail.com>
2023-09-26 12:33:43 -0300

    Usuário autenticado
    Criado metodo para pegar id do usuario no token JWT

--- src/main/java/com/novidades/gestaodeprojetos/service/JWTService.java
+++ src/main/java/com/novidades/gestaodeprojetos/service/JWTService.java
@@ -1,12 +1,15 @@
 package com.novidades.gestaodeprojetos.service;
 
 import java.util.Date;
+import java.util.Optional;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import com.novidades.gestaodeprojetos.model.Usuario;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -16,19 +19,34 @@ public class JWTService {
 
     public String gerarToken(Authentication authentication) {
         int tempoExpiracao = 900000;
-
         Date dataExpiracao = new Date(new Date().getTime() + tempoExpiracao);
 
         // Pega usuário atual da autenticação
-        Usuario usuario = (Usuario) authentication.getPrincipal();
+        Usuario usuarioAtual = (Usuario) authentication.getPrincipal();
 
         String TokenOfJWT = Jwts.builder()
-                .setSubject(usuario.getId().toString())
+                .setSubject(usuarioAtual.getId().toString())
                 .setIssuedAt(new Date())
                 .setExpiration(dataExpiracao)
                 .signWith(SignatureAlgorithm.HS512, chavePrivadaJWT)
                 .compact();
 
         return TokenOfJWT;
+    }
+
+    public Optional<Long> obterIdDoUsuario(String token) {
+        try {
+            // Claims - objeto de autorização
+            Claims claims = parse(token).getBody();
+            Optional<Long> idDoUsuario = Optional.ofNullable(Long.parseLong(claims.getSubject()));
+            return idDoUsuario;
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    // Com a chave pricada, busca no Token as permissões do usuário
+    private Jws<Claims> parse(String token) {
+        return Jwts.parser().setSigningKey(chavePrivadaJWT).parseClaimsJws(token);
     }
 }
